f945fb2c34ef0de8b193444f838b8874eae3bcdc
soeren97 <soeren@langkilde.com>
2023-07-06 20:33:31 +0200

    Added scaling to UMAP plotting

--- explore_data.py
+++ explore_data.py
@@ -1,6 +1,7 @@
 """Explore data using UMAP."""
 import pandas as pd
 import umap
+from sklearn.preprocessing import MinMaxScaler
 
 from source.utils.data import load_data, remove_nan
 from source.utils.plotting import plot_3d
@@ -13,8 +14,12 @@ X_valid, y_valid = remove_nan(X_valid, y_valid)
 X = pd.concat([X_train, X_valid])
 y = pd.concat([y_train, y_valid])
 
+scaler = MinMaxScaler()
+
+X_norm = scaler.fit_transform(X)
+
 umap_model = umap.UMAP(n_components=3)
-umap_result = umap_model.fit_transform(X)
+umap_result = umap_model.fit_transform(X_norm)
 
 umap_df = pd.DataFrame(umap_result, columns=["UMAP 1", "UMAP 2", "UMAP 3"])
 
